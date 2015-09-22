19a21160c6df20be14d3e487804e37e605d55744
Yoga Ramalingam <yramalingam1@bloomberg.net>
2015-09-22 07:55:57 -0400

    Added copyright banner

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,7 @@
+#
+# Cookbook: CollectdWin-cookbook
+#
+
 def default_plugins
   [
     { 'plugin' => { 'attr' => { 'name' => 'Statsd', 'class' => 'BloombergFLP.CollectdWin.StatsdPlugin', 'enable' => true } } },
@@ -89,3 +93,18 @@ default['collectdwin']['plugins']['statsd']['timer']['attr']['upper']           
 default['collectdwin']['plugins']['statsd']['timer']['attr']['sum']                = true
 default['collectdwin']['plugins']['statsd']['timer']['attr']['count']              = true
 default['collectdwin']['plugins']['statsd']['timer']['percentiles']                = default_statsd_timer_percentiles
+
+# ----------------------------------------------------------------------------
+# Copyright (C) 2015 Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ----------------------------- END-OF-FILE ----------------------------------
--- recipes/service_install.rb
+++ recipes/service_install.rb
@@ -1,3 +1,6 @@
+#
+# Cookbook: CollectdWin-cookbook
+#
 
 windows_package node['collectdwin']['service']['name'] do
   action :install
@@ -20,3 +23,18 @@ windows_service node['collectdwin']['service']['name'] do
   action :start
   startup_type :automatic
 end
+
+# ----------------------------------------------------------------------------
+# Copyright (C) 2015 Bloomberg Finance L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# ----------------------------- END-OF-FILE ----------------------------------
