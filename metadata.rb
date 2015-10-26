15e04870bfafde76da6f6c3b1e20159655179bfb
Yoga Ramalingam <yramalingam1@bloomberg.net>
2015-10-26 12:06:19 -0400

    Update version to 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'yramalingam1@bloommberg.net'
 license 'all_rights'
 description 'Installs/Configures CollectdWin-cookbook'
 long_description 'Installs/Configures CollectdWin-cookbook'
-version '0.1.0'
+version '0.1.1'
 
 supports 'windows'
 depends 'poise', '~> 2.2'
