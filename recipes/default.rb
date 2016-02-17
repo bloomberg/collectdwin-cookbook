a61b465fff37f98f419ad2419244b37fed34887e
Josh Engelberg <jengelberg6@bloomberg.net>
2016-02-17 10:54:15 -0500

    Added logic for configuring custom failure actions from downstream cookbooks

--- recipes/default.rb
+++ recipes/default.rb
@@ -32,3 +32,7 @@ windows_service node['collectdwin']['service']['name'] do
   action [:enable, :start]
   startup_type :automatic
 end
+
+if (node['collectdwin']['service'].key?('failure_actions') and node['collectdwin']['service'].key?('failure_reset_time(s)'))
+	include_recipe 'collectdwin::custom_failure_actions'
+end
